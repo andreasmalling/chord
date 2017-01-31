22115eb1cc78c9e1b5509861fba8147748a5c1c9
Kasper Smed <kassmed@cs.au.dk>
2017-01-31 12:09:50 +0100

    Print out args

--- src/main/java/org/sparkle/twilight/Main.java
+++ src/main/java/org/sparkle/twilight/Main.java
@@ -38,6 +38,8 @@ public class Main {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
                 + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        for (String s: args)
+            System.out.println(s);
         System.in.read();
         server.stop();
     }
