8656b7ce99d203522b2a26653d86868b363ac561
Andreas Malling Østergaard <andreas.malling@gmail.com>
2017-02-14 19:23:32 +0100

    Add batch run file

---
+++ run.bat
@@ -0,0 +1,18 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+set /A port=9000
+start cmd /c "mvn exec:java -Dexec.args='8080'"
+pause
+:check
+IF NOT "%port%"=="9015" (Goto loop)
+goto end
+:loop
+echo %port%
+start cmd /c "mvn exec:java "-Dexec.args=%port% 8080""
+set /A port = %port%+1
+timeout 1 > NUL
+goto check
+:end
+echo done
+pause
+)
